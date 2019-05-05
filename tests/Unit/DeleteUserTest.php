41bdb6bdcde46fb544e23879b502efdb6fb33bdc
David Guardia <david.guardia@gmail.com>
2019-05-05 14:44:34 -0400

    Task 14: Create a unit test to update the name of a user in the database to Steve Smith.

---
+++ tests/Unit/DeleteUserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DeleteUserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testDeleteUser()
+    {
+        $user = User::find(4);
+        $this->assertTrue($user->delete());
+    }
+}
